7eb61834a47523d31008d9a24faabf43958eb004
Jack <155328654+Jack-N-S@users.noreply.github.com>
2024-08-28 20:22:42 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var hobbies = ["coding", "skiing", "baseball", "muay thai", "pizza", "gym"];
+//console.log(hobbies.length);
+
+hobbies.push("volleyball");
+//console.log(hobbies[2]);
+
+hobbies.pop();
+//console.log(hobbies);
+
+hobbies.splice(2, 0, "kickball", "dodgeball");
+//console.log(hobbies);
+
+hobbies.shift();
+//console.log(hobbies);
+
+hobbies.unshift("TSLs");
+console.log(hobbies);
